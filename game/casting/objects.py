23d8cb8ebbac325e2c9a1c2209d4039124574f68
awinel <benjamin.huerta.awi@gmail.com>
2022-10-12 16:31:24 -0300

    Main and casting update

---
+++ game/casting/objects.py
@@ -0,0 +1,12 @@
+from game.casting.actor import Actor
+
+class Objects(Actor):
+    """An object that will display a random message or when fund a message about the kitten found.
+
+    The behavior of Artifact is to display a message is time the robot touch an artifact.
+    
+    Attributes:
+        """
+    
+    def __init__(self):
+        super().__init__()
